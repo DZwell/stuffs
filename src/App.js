45834e40e4805c476c6b1c5349eb55b795844739
Daniel Zwelling <dzwellingmusic@gmail.com>
2021-03-18 19:41:36 -0700

    Do front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { v4 as uuid } from 'uuid';
+import { useState } from 'react';
+
+const data = [
+  {
+    name: 'shorts',
+    isPacked: false,
+    id: uuid(),
+  },
+  {
+    name: 'pants',
+    isPacked: false,
+    id: uuid(),
+  },
+  {
+    name: 'sox',
+    isPacked: false,
+    id: uuid(),
+  },
+  {
+    name: 'shirts',
+    isPacked: false,
+    id: uuid(),
+  },
+];
 
 function App() {
+  const [items, setItems] = useState(data);
+
+
+  const handleItemClick = (item) => {
+    item.isPacked = !item.isPacked;
+    setItems([...items]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Stuffs to pack</header>
+      {items.map(item => {
+        return <div className={item.isPacked ? 'packedItem' : ''} onClick={() => handleItemClick(item)} key={item.id}>{item.name}</div>
+      })}
     </div>
   );
 }
